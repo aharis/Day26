c49a34a14edac547828969fcd3952ba1aa8af485
aharis <hare.arapovic@gmail.com>
2021-04-03 08:33:06 +0200

    dodao zadatak ex06

---
+++ ex05/src/App.js
@@ -0,0 +1,13 @@
+import PostRequest from './PostRequest';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <PostRequest />
+    </div>
+  );
+}
+
+export default App;
+
